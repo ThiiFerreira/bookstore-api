d7e4e52e4a4308833d6e0fc4af6a203dc69cedda
Thiago Ferreira <thiago.ferreira@aluno.ifsp.edu.br>
2022-02-15 13:09:35 -0300

    findById para Categoria

---
+++ src/main/java/com/thiago/bookstore/service/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.thiago.bookstore.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thiago.bookstore.domain.Categoria;
+import com.thiago.bookstore.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repository;
+	
+	public Categoria findById(Integer id) {
+		
+		Optional<Categoria> obj = repository.findById(id);
+		return obj.orElse(null);		
+	}
+}
