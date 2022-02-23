41298a79a94fa297d6d06f70fffbfc22536086e2
Thiago Ferreira <thiago.ferreira@aluno.ifsp.edu.br>
2022-02-23 00:04:58 -0300

    POST para Livro

--- src/main/java/com/thiago/bookstore/service/LivroService.java
+++ src/main/java/com/thiago/bookstore/service/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.thiago.bookstore.domain.Categoria;
 import com.thiago.bookstore.domain.Livro;
 import com.thiago.bookstore.repositories.LivroRepository;
 import com.thiago.bookstore.service.exceptions.ObjectNotFoundException;
@@ -45,6 +46,13 @@ public class LivroService {
 		newObj.setTitulo(obj.getTitulo());
 		newObj.setNome_autor(obj.getNome_autor());
 		newObj.setTexto(obj.getTexto());
+	}
+	//recebendo os dados do novo livro
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat);
+		obj.setCategoria(cat);
+		return repository.save(obj);
 	}
 
 }
